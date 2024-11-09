97642c0e1efa1515f085965cd5058afddb56d406
Cory Fail <me@coryfail.com>
2024-11-09 12:27:53 -0500

    Add skeleton 3.0 beta

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
-/** @type {import('tailwindcss').Config} */
+import { join } from "path";
+import { skeleton } from "@skeletonlabs/skeleton/plugin";
+import * as themes from "@skeletonlabs/skeleton/themes";
+
+/** @type {import('tailwindcss').Config} \*/
 export default {
-    content: ["./src/**/*.{html,js,svelte,ts}"],
+    content: [
+        "./src/**/*.{html,js,svelte,ts}",
+        join(
+            require.resolve("@skeletonlabs/skeleton-svelte"),
+            "../**/*.{html,js,svelte,ts}"
+        ),
+    ],
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: [
+        skeleton({
+            // NOTE: each theme included will be added to your CSS bundle
+            themes: [themes.cerberus, themes.rose],
+        }),
+    ],
 };
